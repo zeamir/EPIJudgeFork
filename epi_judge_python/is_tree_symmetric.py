287e0eac90a762e0b77e0810d36b9070b0196b19
Amir.Zahavi <amir.zahavi@cyberark.com>
2021-12-26 07:05:03 +0200

    solve 'Test if a binary tree is symmetric' (problem 9.2)

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -2,9 +2,26 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
+# 09:15
+# 09:22 finished writing code
+# 09:23 finished debugging. all working 1st shot :)
 def is_symmetric(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    if not tree:
+        return True
+    return check_symmetric(tree.left, tree.right)
+
+
+def check_symmetric(left: BinaryTreeNode, right: BinaryTreeNode) -> bool:
+    if not left and not right:
+        return True
+    elif left and right:
+        # compare opposite child nodes
+        return left.data == right.data and \
+               check_symmetric(left.left, right.right) and \
+               check_symmetric(left.right, right.left)
+    else:
+        # only one of the child nodes is missing
+        return False
 
 
 if __name__ == '__main__':
