ad5746501ddcc821af4a0f0ee879d1c5fb9db777
Amir.Zahavi <amir.zahavi@cyberark.com>
2021-12-26 07:04:40 +0200

    solve 'Test if a binary tree is height-balanced' (problem 9.1)

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,10 +1,43 @@
+# also available on leetcode
+# ש# #https://leetcode.com/problems/balanced-binary-tree/submissions/
+from typing import NamedTuple
+
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
+class IsBalancedRes(NamedTuple):
+    is_balanced: bool
+    height: int
+
+
+# @dataclasses.dataclass
+# class IsBalancedRes:
+#     is_balanced: bool
+#     height: int
+
+
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    res = is_balanced(tree)
+    return res.is_balanced
+
+
+def is_balanced(node: BinaryTreeNode) -> IsBalancedRes:
+    """
+    returns Tuple of is_balanced, left height, right height
+    """
+    if node is None:
+        return IsBalancedRes(True, -1)
+    res_left = is_balanced(node.left)
+    if not res_left.is_balanced:
+        return res_left
+    res_right = is_balanced(node.right)
+    if not res_right.is_balanced:
+        return res_right
+    if abs(res_left.height - res_right.height) <= 1:
+        return IsBalancedRes(True, 1 + max(res_left.height, res_right.height))
+    else:
+        return IsBalancedRes(False, 0)
 
 
 if __name__ == '__main__':
