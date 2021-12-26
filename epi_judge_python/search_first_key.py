22f8c3bb08872ce2751cab65ab41378b57eed5e7
Amir.Zahavi <amir.zahavi@cyberark.com>
2021-12-26 07:07:05 +0200

    solve problem #11.1 - Search a sorted array for first occurrence of k

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -3,9 +3,26 @@ from typing import List
 from test_framework import generic_test
 
 
+# writing time: 5 minutes:
+# debug time: 2 min
 def search_first_of_k(A: List[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    s = 0
+    e = len(A) - 1
+    # find first index
+    first_index = -1
+    while s <= e:
+        m = int((s + e) / 2)
+        if A[m] == k:
+            first_index = m
+            # first index may still be in left part
+            e = m - 1
+        elif A[m] < k:
+            # need to keep looking to the right
+            s = m + 1
+        else:  # A[m] > k. need to keep looking to the left
+            e = m - 1
+
+    return first_index
 
 
 if __name__ == '__main__':
